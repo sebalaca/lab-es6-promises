a494def2bd38dd8c28e38d2f3d27c8b9bea237ee
Sebastian <seba.laca@gmail.com>
2022-10-23 16:40:54 +0200

    Update labs

--- javascript/index.js
+++ javascript/index.js
@@ -83,11 +83,43 @@ obtainInstruction('steak', 0)
     document.querySelector("#steak").innerHTML += `<li>Stake is ready!</li>`;
     document.querySelector("#steakImg").removeAttribute("hidden");
   })
-  // ... Your code here
   .catch(error => console.log(error))
 
 // Iteration 3 using async/await
-// ...
+async function makeBroccoli() {
+  for(let i=0; i<broccoli.length; i++){
+    let step = await obtainInstruction('broccoli',i)
+    document.getElementById('broccoli').innerHTML += `<li>${step}</li>`
+  }
+  document.getElementById('broccoli').innerHTML += `<li>Broccoli is ready!</li>`
+  document.querySelector("#broccoliImg").removeAttribute("hidden");
+}
+
+makeBroccoli()
 
 // Bonus 2 - Promise all
-// ...
+const step0 = obtainInstruction('brusselsSprouts', 0)
+const step1 = obtainInstruction('brusselsSprouts', 1)
+const step2 = obtainInstruction('brusselsSprouts', 2)
+const step3 = obtainInstruction('brusselsSprouts', 3)
+const step4 = obtainInstruction('brusselsSprouts', 4)
+const step5 = obtainInstruction('brusselsSprouts', 5)
+const step6 = obtainInstruction('brusselsSprouts', 6)
+const step7 = obtainInstruction('brusselsSprouts', 7)
+
+Promise.all( [step0,
+  step1,
+  step2,
+  step3,
+  step4,
+  step5,
+  step6,
+  step7])
+
+  .then((step) => {
+    step.forEach((step) => {
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>${step}</li>`;
+    })
+    document.getElementById('brusselsSprouts').innerHTML += `<li>Brussels sprouts are ready!</li>`
+    document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
+  })
